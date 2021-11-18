3fe713581620a8a0f509526339dd187b786bc4d7
Haibo Jiang <jianghaibo12138@gmail.com>
2021-11-18 16:13:08 +0800

    Add more http api

--- src/main/java/com/amazing2j/amz/utils/okhttp/OkHttpUtil.java
+++ src/main/java/com/amazing2j/amz/utils/okhttp/OkHttpUtil.java
@@ -1,12 +1,14 @@
 package com.amazing2j.amz.utils.okhttp;
 
 import com.alibaba.fastjson.JSON;
+import com.amazing2j.amz.exception.HTTPException;
 import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -20,27 +22,112 @@ public class OkHttpUtil {
             .readTimeout(8000, TimeUnit.MILLISECONDS)
             .build();
 
-    private byte[] sendPostRequest(String reqUrl, Request.Builder builder) throws IOException {
+    private String request(String reqUrl, Request.Builder builder) throws IOException {
         long startTime = System.currentTimeMillis();
-        byte[] body = new byte[]{};
+        String body = null;
         Response response = okHttpClient.newCall(builder.build()).execute();
+        if (response.code() != 200) {
+            throw new HTTPException(response.code(), response.message());
+        }
         ResponseBody responseBody = response.body();
         if (responseBody != null) {
-            body = responseBody.bytes();
+            body = responseBody.string();
         }
         long endTime = System.currentTimeMillis();
         logger.info("{} cost time:{}", reqUrl.substring(reqUrl.lastIndexOf("/") + 1), (endTime - startTime));
         return body;
     }
 
-    public byte[] sendPostByJson(String reqUrl, Map<String, String> headers, Object param) throws IOException {
-        String bodyStr = JSON.toJSONString(param);
+    private String sendPostRequest(String reqUrl, Map<String, String> headers, Object data, String dataFormat) throws IOException {
+        String bodyStr = JSON.toJSONString(data);
         Request.Builder requestBuilder = new Request.Builder().url(reqUrl);
         for (String key : headers.keySet()) {
             requestBuilder.addHeader(key, headers.get(key));
         }
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), bodyStr);
+        if (dataFormat == null) {
+            dataFormat = "application/json";
+        }
+        RequestBody requestBody = RequestBody.create(MediaType.parse(dataFormat), bodyStr);
         Request.Builder builder = requestBuilder.post(requestBody);
-        return sendPostRequest(reqUrl, builder);
+        return request(reqUrl, builder);
+    }
+
+    private String sendPutRequest(String reqUrl, Map<String, String> headers, Object data, String dataFormat) throws IOException {
+        String bodyStr = JSON.toJSONString(data);
+        Request.Builder requestBuilder = new Request.Builder().url(reqUrl);
+        for (String key : headers.keySet()) {
+            requestBuilder.addHeader(key, headers.get(key));
+        }
+        if (dataFormat == null) {
+            dataFormat = "application/json";
+        }
+        RequestBody requestBody = RequestBody.create(MediaType.parse(dataFormat), bodyStr);
+        Request.Builder builder = requestBuilder.put(requestBody);
+        return request(reqUrl, builder);
+    }
+
+    private String pckUrlParam(String reqUrl, Map<String, Object> params) {
+        if (params == null) {
+            return reqUrl;
+        }
+        String paraStr = null;
+        for (String key : params.keySet()) {
+            paraStr = String.format("%s=%s", key, params.get(key));
+        }
+        if (paraStr != null) {
+            reqUrl = String.format("%s?%s", reqUrl, paraStr);
+        }
+        return reqUrl;
+    }
+
+    private String pckUrlRestParam(String reqUrl, Object[] params) {
+        if (params == null) {
+            return reqUrl;
+        }
+        ArrayList<String> ps = new ArrayList<>();
+        for (Object p : params) {
+            ps.add(p.toString());
+        }
+        return String.format("%s/%s", reqUrl, String.join("/", ps));
+    }
+
+    public String sendGetRequest(String reqUrl, Map<String, String> headers, Map<String, Object> params) throws IOException {
+        reqUrl = pckUrlParam(reqUrl, params);
+        Request.Builder requestBuilder = new Request.Builder().url(reqUrl);
+        for (String key : headers.keySet()) {
+            requestBuilder.addHeader(key, headers.get(key));
+        }
+        Request.Builder builder = requestBuilder.get();
+        return request(reqUrl, builder);
+    }
+
+    public String sendGetRequestRest(String reqUrl, Map<String, String> headers, String[] params) throws IOException {
+        reqUrl = pckUrlRestParam(reqUrl, params);
+        Request.Builder requestBuilder = new Request.Builder().url(reqUrl);
+        for (String key : headers.keySet()) {
+            requestBuilder.addHeader(key, headers.get(key));
+        }
+        Request.Builder builder = requestBuilder.get();
+        return request(reqUrl, builder);
+    }
+
+    public String sendPostRequestRestByJson(String reqUrl, Map<String, String> headers, Object[] params, Object data) throws IOException {
+        reqUrl = pckUrlRestParam(reqUrl, params);
+        return sendPostRequest(reqUrl, headers, data, "application/json");
+    }
+
+    public String sendPostRequestByJson(String reqUrl, Map<String, String> headers, Map<String, Object> params, Object data) throws IOException {
+        reqUrl = pckUrlParam(reqUrl, params);
+        return sendPostRequest(reqUrl, headers, data, "application/json");
+    }
+
+    public String sendPutRequestRestByJson(String reqUrl, Map<String, String> headers, Object[] params, Object data) throws IOException {
+        reqUrl = pckUrlRestParam(reqUrl, params);
+        return sendPutRequest(reqUrl, headers, data, "application/json");
+    }
+
+    public String sendPutRequestByJson(String reqUrl, Map<String, String> headers, Map<String, Object> params, Object data) throws IOException {
+        reqUrl = pckUrlParam(reqUrl, params);
+        return sendPutRequest(reqUrl, headers, data, "application/json");
     }
 }
